0d02de3721eb95b7df9c8cbffb0ab687729c6431
Thor Erik Lie <thor@otc.nu>
2014-01-24 13:38:48 +0100

    removed fish as I opted for zsh instead

--- install.sh
+++ install.sh
@@ -5,22 +5,6 @@ function link()
     ln -sf "${PWD}/$1" "$HOME/.$1"
 }
 
-# for linking entire directories in .config
-function configLink()
-{
-	ln -sf "${PWD}/config/$1" "$HOME/.config/$1"
-}
-
-# for linking fish files (as fish creates the .config/fish/ folder with it's current pid when it's active)
-function fishLink()
-{
-	ln -sf "${PWD}/config/fish/$1" "$HOME/.config/fish/$1"
-}
-function fishCp()
-{
-	cp "${PWD}/config/fish/$1" "$HOME/.config/fish/$1"
-}
-
 # Directories
 [ ! -e ~/.conky/ ] && link conky
 
@@ -31,5 +15,3 @@ link vimrc
 link gitignore
 link gitconfig
 link conkyrc
-fishLink config.fish
-[ ! -e $HOME/.config/fish/machine.fish ] && fishCp machine.fish
